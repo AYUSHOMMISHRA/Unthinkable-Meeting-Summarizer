26fd21167449c00e64e4f2ca474ab548111dd22b
AYUSH MISHRA <connect.ayushommishra@gmail.com>
2025-10-15 15:23:55 +0530

    feat(config): add __init__.py for config package initialization

---
+++ config/__init__.py
@@ -0,0 +1,10 @@
+"""
+Meeting Summarizer Configuration Package
+
+This package contains Django configuration files for the Meeting Summarizer application.
+"""
+
+# This will make Celery app available when Django starts (if using Celery)
+# from .celery import app as celery_app
+
+# __all__ = ('celery_app',)
